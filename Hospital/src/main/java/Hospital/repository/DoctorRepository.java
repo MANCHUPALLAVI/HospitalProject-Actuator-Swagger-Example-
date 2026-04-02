49d5da5f79f9389b68e453314df1e5410f67006e
MANCHU PALLAVI <146466816+MANCHUPALLAVI@users.noreply.github.com>
2026-04-02 22:05:58 +0530

    DoctorRepository.java

--- Hospital/src/main/java/Hospital/repository/DoctorRepository.java
+++ Hospital/src/main/java/Hospital/repository/DoctorRepository.java
@@ -2,6 +2,7 @@ package Hospital.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import Hospital.entity.Doctor;
+@Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long>
 {
 
