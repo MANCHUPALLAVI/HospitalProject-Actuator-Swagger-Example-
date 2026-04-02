c9f62b064499064f9f17a154a08c58967f31116f
MANCHU PALLAVI <146466816+MANCHUPALLAVI@users.noreply.github.com>
2026-04-02 22:05:39 +0530

    AppointmentRepository.java

--- Hospital/src/main/java/Hospital/repository/AppointmentRepository.java
+++ Hospital/src/main/java/Hospital/repository/AppointmentRepository.java
@@ -2,6 +2,7 @@ package Hospital.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import Hospital.entity.Appointment;
+@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>
 {
 
