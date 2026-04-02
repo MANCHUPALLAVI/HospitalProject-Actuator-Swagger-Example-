4634a0114a255fb4395d9b1cca9ba08afbd012a6
MANCHU PALLAVI <146466816+MANCHUPALLAVI@users.noreply.github.com>
2026-04-02 22:06:14 +0530

    PatientRepository.java

--- Hospital/src/main/java/Hospital/repository/PatientRepository.java
+++ Hospital/src/main/java/Hospital/repository/PatientRepository.java
@@ -2,7 +2,7 @@ package Hospital.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import Hospital.entity.Patient;
-
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Long>{
 
 }
